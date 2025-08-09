7c6369a1547e0dcce0ebfa9d7f3f37f5f2dd5b6e
Minsung Kim <mskim4048@naver.com>
2025-08-09 20:05:02 +0900

    Initial Commit

---
+++ SeSAC-Assignment-31/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-Assignment-31
+//
+//  Created by 김민성 on 8/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
