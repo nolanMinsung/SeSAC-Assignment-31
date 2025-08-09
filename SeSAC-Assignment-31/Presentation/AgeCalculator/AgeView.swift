e2b1977d95f4955a863c1909f7b994aeeb4314b4
Minsung Kim <mskim4048@naver.com>
2025-08-09 22:41:16 +0900

    feat: AgeViewController에서 view 분리

---
+++ SeSAC-Assignment-31/Presentation/AgeCalculator/AgeView.swift
@@ -0,0 +1,77 @@
+//
+//  AgeView.swift
+//  SeSAC-Assignment-31
+//
+//  Created by 김민성 on 8/9/25.
+//
+
+import UIKit
+
+import SnapKit
+
+final class AgeView: UIView {
+    
+    let textField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "나이를 입력해주세요"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    let resultButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.setTitle( "클릭", for: .normal)
+        button.layer.cornerRadius = 8
+        return button
+    }()
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "여기에 결과를 보여주세요"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
+extension AgeView {
+    
+    func configureHierarchy() {
+        addSubview(textField)
+        addSubview(resultButton)
+        addSubview(label)
+    }
+    
+    func configureLayout() {
+        textField.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide).offset(20)
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.height.equalTo(44)
+        }
+        
+        resultButton.snp.makeConstraints { make in
+            make.top.equalTo(textField.snp.bottom).offset(20)
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.height.equalTo(44)
+        }
+        
+        label.snp.makeConstraints { make in
+            make.top.equalTo(resultButton.snp.bottom).offset(20)
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.height.equalTo(44)
+        }
+    }
+    
+}
--- SeSAC-Assignment-31/Presentation/AgeCalculator/AgeViewController.swift
+++ SeSAC-Assignment-31/Presentation/AgeCalculator/AgeViewController.swift
@@ -8,58 +8,17 @@
 import UIKit
 
 class AgeViewController: UIViewController {
-    let textField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "나이를 입력해주세요"
-        textField.borderStyle = .roundedRect
-        return textField
-    }()
-    let resultButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemBlue
-        button.setTitle( "클릭", for: .normal)
-        button.layer.cornerRadius = 8
-        return button
-    }()
-    let label: UILabel = {
-        let label = UILabel()
-        label.text = "여기에 결과를 보여주세요"
-        label.textAlignment = .center
-        return label
-    }()
+    
+    let rootView = AgeView()
+    
+    override func loadView() {
+        view = rootView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureHierarchy()
-        configureLayout()
         
-        resultButton.addTarget(self, action: #selector(resultButtonTapped), for: .touchUpInside)
-    }
-    
-    func configureHierarchy() {
-        view.addSubview(textField)
-        view.addSubview(resultButton)
-        view.addSubview(label)
-    }
-    
-    func configureLayout() {
-        textField.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
-            make.horizontalEdges.equalToSuperview().inset(20)
-            make.height.equalTo(44)
-        }
-        
-        resultButton.snp.makeConstraints { make in
-            make.top.equalTo(textField.snp.bottom).offset(20)
-            make.horizontalEdges.equalToSuperview().inset(20)
-            make.height.equalTo(44)
-        }
-        
-        label.snp.makeConstraints { make in
-            make.top.equalTo(resultButton.snp.bottom).offset(20)
-            make.horizontalEdges.equalToSuperview().inset(20)
-            make.height.equalTo(44)
-        }
+        rootView.resultButton.addTarget(self, action: #selector(resultButtonTapped), for: .touchUpInside)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -70,10 +29,10 @@ class AgeViewController: UIViewController {
         view.endEditing(true)
         
         do {
-            let validAge: Int = try getValidAgeInput(input: textField.text!)
-            label.text = "당신의 나이는 \(validAge)입니다."
+            let validAge: Int = try getValidAgeInput(input: rootView.textField.text!)
+            rootView.label.text = "당신의 나이는 \(validAge)입니다."
         } catch {
-            label.text = error.localizedDescription
+            rootView.label.text = error.localizedDescription
         }
         
     }
